eb4b48c43779d3455fd60445f8ebab2d260fef77
DevEngTheCloud <96835451+DevEngTheCloud@users.noreply.github.com>
2024-10-30 12:07:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'Hello Group this is workshop 2, test finally working! Push fail'
+   return 'Hello Group this is workshop 2, test finally working! Push fail 1207#'
